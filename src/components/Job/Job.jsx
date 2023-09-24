97fbc09e23fa1023c8a7755d08f0282b30d719f1
Saiful Islam <98213668+azadgmtl@users.noreply.github.com>
2023-09-24 22:28:19 +0600

    changed

--- src/components/Job/Job.jsx
+++ src/components/Job/Job.jsx
@@ -9,7 +9,7 @@ const Job = ({job}) => {
 
     return (
         <div className="card card-compact w-96 bg-base-100 shadow-md px-3 py-8 border-2 rounded-md text-end">
-            <img  className="object-left-top w-32 h-10 mx-4 mb-4" src={logo} alt="Shoes" />
+            <img  className="object-left-top w-32 h-20 mx-4 mb-4" src={logo} alt="Shoes" />
             <div className="card-body text-start">
                 <h2 className="card-title font-bold text-slate-600 text-2xl"> {job_title} </h2>
                 <p className="text-slate-500 font-normal text-lg"> {company_name} </p>
